26ba9449950ec73ef46c09cd8ffe31e3635c2c48
Carsten <carsten@krokant.be>
2023-07-17 14:26:26 +0200

    React admin UI

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './footer.scss';
+
+const Footer = () => {
+	return (
+		<div className='footer'>
+			<span>Your admin UI</span>
+			<span>© My Company 2023</span>
+		</div>
+	);
+}
+
+export default Footer;
